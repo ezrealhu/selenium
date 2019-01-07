50415eeca9920a5c383ccb0e67b30e3a8f559790
huyin <363468616@qq.com>
2019-01-07 13:34:51 +0800

    selenium脚本

--- test_case/models/myunit.py
+++ test_case/models/myunit.py
@@ -3,7 +3,7 @@
 
 from selenium import webdriver
 import unittest
-import time, os
+import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -13,15 +13,8 @@ class MyTest(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
-        self.driver.get("http://115.236.67.13:9000/zdzz")  #测试网址
+        self.driver.get("http://115.236.67.13:9000/zdzz")  #公司内网测试网址
         time.sleep(1)
-
-    def assertAndScreenshot(self, a, b):
-        if a == b:
-            print("测试通过！")
-        else:
-            print('标题不等于“%s” 测试不通过!' % b)
-            self.driver.get_screenshot_as_file("E:\\PycharmProjects\\selenium-test\\report\\1.jpg")
 
     def tearDown(self):
         self.driver.quit()
@@ -32,11 +25,3 @@ def wait_title(self, location):   #通过判断title的方式,封装显式等待
     except:
             "等待页面时间超时！"
     return
-
-
-def assertAndScreenshot(self, a, b):
-    if a == b:
-        print("测试通过！")
-    else:
-        print('标题不等于“%s” 测试不通过!' % b)
-        self.driver.get_screenshot_as_file("E:\\PycharmProjects\\selenium-test\\report\\1.jpg")
